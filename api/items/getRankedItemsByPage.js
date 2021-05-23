587860eab92b148ea1a235f1d310f59bb26a81d1
Logos106 <logos106@outlook.com>
2021-05-23 09:45:20 +0900

    auth changed

--- api/items/getRankedItemsByPage.js
+++ api/items/getRankedItemsByPage.js
@@ -10,38 +10,35 @@ export default async function getRankedItemsByPage(page, signedIn) {
   const itemsPerPage = config.itemsPerPage
   const commentsPerPage = config.commentsPerPage
 
-  // Instantiate a new Directus object
-  const directus = new Directus(credential.baseURL)
+  const directus = credential.directus
 
-  // Login to Directus
-  // await directus.auth.login({
-  //   email: credential.email,
-  //   password: credential.password,
-  // });
-await directus.auth.refresh();
   // Fetch items with conditions
   try {
     const startDate = moment().unix() - (86400 * maxAgeOfRankedItemsInDays)
     const items = directus.items('items')
-    const result = await items.readMany({
-    	filter: {
-    		created: {
-    			_gte: startDate,
-    		},
-        dead: {
-          _eq: false
-        }
-    	},
-      skip: (page - 1) * itemsPerPage,
-      take: itemsPerPage
-    });
 
-    return {
-      success: true,
-      items: result.data,
-      isMore: false, //totalItemCount > (((page - 1) * itemsPerPage) + itemsPerPage) ? true : false
-      getDataError: false
+    if (!signedIn) {  // If he is a guest
+      const result = await items.readMany({
+        filter: {
+          created: {
+            _gte: startDate,
+          },
+          dead: {
+            _eq: false
+          }
+        },
+        skip: (page - 1) * itemsPerPage,
+        take: itemsPerPage
+      });
+
+      return {
+        success: true,
+        items: result.data,
+        isMore: false, //totalItemCount > (((page - 1) * itemsPerPage) + itemsPerPage) ? true : false
+        getDataError: false
+      }
     }
+
   } catch(error) {
     return {getDataError: true}
   }
