d58fca1d804e456f0f8b80eedc1d61f5dd03565b
Logos106 <logos106@outlook.com>
2021-06-04 18:05:57 +0900

    a

--- pages/api/login.js
+++ pages/api/login.js
@@ -50,7 +50,7 @@ export default async function handler(req, res) {
     return res.status(200).json({ success: true })
   } catch (error) {
 
-    res.status(200).json({ submitError: true })
+    res.status(200).json({ credentialError: true })
   }
 
 }
